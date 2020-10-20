325d9a98e72f11c7de02b584d7d5b23900830ea9
jpca999 <jpca999@gmail.com>
2020-10-19 22:05:00 -0700

    minfor progress

--- screens/AllRecords.tsx
+++ screens/AllRecords.tsx
@@ -5,6 +5,21 @@ import { API, graphqlOperation } from 'aws-amplify'
 import { onCreateRecord, onDeleteRecord, onUpdateRecord } from '../graphql/subscriptions'
 import { useNavigation } from '@react-navigation/native';
 
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
+
 
 
 class AllRecords extends React.Component {
@@ -23,6 +38,7 @@ class AllRecords extends React.Component {
     const result = await API.graphql (graphqlOperation(listRecords))
     console.log(' 32- result = ', result ); 
     this.setState ({ psosts: result.data.listRecords.items})
+
 };
 
   componentDidMount() {
@@ -32,10 +48,41 @@ class AllRecords extends React.Component {
 
   render(props) {
     // const { navigate } = this.props.props.navigation;
-    return (<View>
-           <Text>  Hi, All records </Text>
-      </View>
-    )
+
+    const Item = ({ item, onPress, style }) => (
+      <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+        <Text style={styles.title}>{item.title}</Text>
+      </TouchableOpacity>
+    );
+
+
+    // const [selectedId, setSelectedId] = useState(null);
+    const handleSelect = () =>{
+      console.log('hanlde slected clcieked'); 
+    }
+
+    const renderItem = ({ item }) => {
+      // const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+
+      return (
+        <Item
+          item={item}
+          onPress={() => handleSelect() }
+        />
+      );
+    };
+  
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          // data={this.props.posts}
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          // extraData={selectedId}
+        />
+      </SafeAreaView>
+    );
   }
 }
 
