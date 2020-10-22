f086a4305b77d172ead1f86f2aee701d69e6644a
jpca999 <jpca999@gmail.com>
2020-10-21 22:31:38 -0700

    -

--- screens/HooksForm.tsx
+++ screens/HooksForm.tsx
@@ -1,10 +1,9 @@
-
 // export default function HooksForm(props) {
 
 
 import React, { useState, Component } from 'react';
 import { ListItem, Icon, Avatar } from 'react-native-elements'
-import { View, Text, SafeAreaView, ScrollView, StyleSheet  } from 'react-native'
+import { View, Text, SafeAreaView, KeyboardAvoidingView,  ScrollView, StyleSheet } from 'react-native'
 import { Card, CardSection, Input } from '../components/common';
 import { Column as Col, Row } from 'react-native-flexbox-grid'
 import { API, graphqlOperation, Auth } from 'aws-amplify'
@@ -14,98 +13,309 @@ import { TextInput, Button, Alert } from "react-native";
 import { useForm, Controller } from "react-hook-form";
 
 export default function HooksForm(props) {
-  const { control, handleSubmit, errors } = useForm();
+const { control, handleSubmit, errors } = useForm();
 
-    const onSubmit = async (data) => {
-        console.log(data);
-        const input = {
-            id: props.route.params.item.id,
-             companyName: data.companyName,
-             executiveFirstName: data.executiveFirstName,
-            //  lastUpdated: new Date().toISOString() 
-        }
-        console.log(input);
-        await API.graphql(graphqlOperation(updateRecord, { input }))
-   }      
+const onSubmit = async (data) => {
+console.log(data);
+const input = {
+id: props.route.params.item.id,
+companyName: data.companyName,
+executiveFirstName: data.executiveFirstName,
+executiveLastName: data.executiveLastName,
+contactPerson: data.contactPerson,
+followupDate: data.followupDate,
+comment: data.comment,
+email: data.email,
+status: data.status,
+priorities: data.priorities,
+phone_no: data.phone_no,
+address: data.address,
+cell_phone: data.cell_phone,
+city: data.city,
+state: data.state,
+ownerStatus: data.ownerStatus,
+Linkedin: data.Linkedin,
+business_size: data.business_size,
+// lastUpdated: new Date().toISOString()
+}
+console.log(input);
+await API.graphql(graphqlOperation(updateRecord, { input }))
+}
 
-  return (
+return (
+<SafeAreaView>
 <ScrollView>
-    <View>  
-      <Card>
-      <Controller
-        control={control}
-        render={({ onChange, onBlur, value }) => (
-        <CardSection>  
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
+<KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+     
+    >
+  <View>
+    <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    <Card>
+      <Text  style={styles.textStyle}> Company Name</Text>
+      <CardSection>       
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
             value={value}
-          />
-          </CardSection>
-        )}
-        name="companyName"
-        rules={{ required: true }}
-        defaultValue={props.route.params.item.companyName}
-      />
+            />
+            )}
+            name="companyName"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.companyName}
+            />
+            </View>
+      </CardSection>
 
- <CardSection>
-   
-      <Controller
-        control={control}
-        render={({ onChange, onBlur, value }) => (
-
-        <CardSection>  
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
+      <Text style={styles.textStyle}> Exe First Name</Text>
+      <CardSection>
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
             value={value}
-          />
-        </CardSection>  
-        )}
-        name="executiveFirstName"
-        defaultValue={props.route.params.item.executiveFirstName}
-      />
- </CardSection>
-</Card>             
+            />
+            )}
+            name="executiveFirstName"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.executiveFirstName}
+            />
+            </View>
+      </CardSection>
 
-      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-    </View>
+      <Text style={styles.textStyle}> Last Name</Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="executiveLastName"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.executiveLastName}
+            />
+            </View>
+      </CardSection>
+
+
+      <Text style={styles.textStyle}> Contact Person</Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="contactPerson"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.contactPerson}
+            />
+            </View>
+      </CardSection>
+
+
+      <Text style={styles.textStyle}> Phone No</Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="phone_no"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.phone_no}
+            />
+            </View>
+      </CardSection> 
+
+
+ <Text style={styles.textStyle}> Status</Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="status"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.status}
+            />
+            </View>
+      </CardSection>  
+
+
+ <Text style={styles.textStyle}> Direct Cell </Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="cell_phone"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.cell_phone}
+            />
+            </View>
+      </CardSection>  
+
+
+
+ <Text style={styles.textStyle}> Owner Status</Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="ownerStatus"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.ownerStatus}
+            />
+            </View>
+      </CardSection>   
+
+
+      <Text style={styles.textStyle}> Follow up Date</Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="followupDate"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.followupDate}
+            />
+            </View>
+      </CardSection>   
+
+
+      <Text style={styles.textStyle}> Comment</Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="comment"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.comment}
+            />
+            </View>
+      </CardSection>   
+
+
+      <Text style={styles.textStyle}> Email </Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="email"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.email}
+            />
+            </View>
+      </CardSection>   
+
+
+      <Text style={styles.textStyle}> City  </Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="city"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.city}
+            />
+            </View>
+      </CardSection>   
+
+
+      <Text style={styles.textStyle}> Street Address  </Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="address"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.address}
+            />
+            </View>
+      </CardSection>   
+
+
+      <Text style={styles.textStyle}> State </Text>
+      <CardSection>        
+      <View style={styles.viewContainerStyle}>
+        <Controller control={control} render={({ onChange, onBlur, value })=> (
+          <TextInput style={styles.input} onBlur={onBlur} onChangeText={value=> onChange(value)}
+            value={value}
+            />
+            )}
+            name="state"
+            // rules={{ required: true }}
+            defaultValue={props.route.params.item.state}
+            />
+            </View>
+      </CardSection>  
+
+    </Card>
+
+  </View>
+  </KeyboardAvoidingView>
 </ScrollView>
+</SafeAreaView>  
 
-  );
+);
 }
 
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginBottom: 15,
-    },
-    error: {
-      marginBottom: 20,
-      height: 17.5,
-    },
-    viewContainerStyle:{
-      height: 40,
-      flex: 1,
-      flexDirection: 'row',
-      alignItems: 'center',
-      // width: width,
-    },
-    input:{
-    color: '#000',
-    backgroundColor: '#FFFFFF',
-    paddingRight: 5,
-    paddingLeft: 5,
-    fontSize: 18,
-    lineHeight: 23,
-    flex: 2
-    }
-  });
-
-  
+        textStyle:{
+          color: '#8795fa'
+        },
+        container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 15,
+        },
+        error: {
+        marginBottom: 20,
+        height: 17.5,
+        },
+        viewContainerStyle:{
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        // width: width,
+        },
+        input:{
+        // color: '#000',
+        // color:'#5E90AF',
+        color:'#184662',
+        backgroundColor: '#FFFFFF',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2
+        }
+});
